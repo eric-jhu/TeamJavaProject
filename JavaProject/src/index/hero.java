502a2ab5aba846388be1d89def3fb336588abc11
Kanice0207 <146327166+Kanice0207@users.noreply.github.com>
2023-10-05 17:48:13 +0800

    Add files via upload

---
+++ JavaProject/src/index/hero.java
@@ -0,0 +1,11 @@
+package index;
+
+public class hero {
+
+	static int swordsman_hp = 90;//劍士血量
+	static int swordsman_attack = 50;//劍士攻擊力
+	static int mage_hp = 70;//法師血量
+	static int mage_attack = 80;//法師攻擊力
+	
+
+}
